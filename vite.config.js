3543234ca963069a341cfbd3626a7b311e402548
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-01-09 09:44:20 +0100

    No certificate required for building.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-import { readFileSync } from 'fs'
+import { readFileSync, existsSync } from 'fs'
 // https://vitejs.dev/config/
 // https://stackoverflow.com/questions/69417788/vite-https-on-localhost
 // https://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
+
+function getCertificate(path) {
+  return existsSync(path) ? readFileSync(path) : '';
+}
 export default defineConfig({
   plugins: [react()],
 //   build: {
@@ -18,8 +22,8 @@ export default defineConfig({
     host: '127.0.0.1',
     port: 5173,
     https: {
-      key: readFileSync('cert/private-key.pem'),
-      cert: readFileSync('cert/certificate.pem')
+      key: getCertificate('cert/private-key.pem'),
+      cert: getCertificate('cert/certificate.pem')
     }
   }
   

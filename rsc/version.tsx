0f655858aab58dacc95a9f2230d70b36a47988ab
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-01-23 15:56:25 +0100

    [Automated]: Increase version to 0.3rc10

--- rsc/version.tsx
+++ rsc/version.tsx
@@ -2,7 +2,7 @@
 // Automatically generated version file.
 // Please do not remove this file manually.
 
-const VAYS_VERSION = '0.3rc9';
+const VAYS_VERSION = '0.3rc10';
 export default VAYS_VERSION;
 
 

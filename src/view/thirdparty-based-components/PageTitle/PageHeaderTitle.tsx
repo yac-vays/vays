ac8b2f3b5610cbe69b725f2b28644255617943d4
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-01-24 11:40:12 +0100

    Modify title styling

--- src/view/thirdparty-based-components/PageTitle/PageHeaderTitle.tsx
+++ src/view/thirdparty-based-components/PageTitle/PageHeaderTitle.tsx
@@ -10,9 +10,11 @@ const PageHeaderTitle = ({ title, subText, children }: PageHeaderTitleProps) => 
       <div className="mb-5 flex items-center justify-between">
         <div>
           <div className="flex flex-row" style={{ marginLeft: 0 }}>
-            <h2 className="mb-1.5 text-title-md2 font-bold text-black dark:text-white">{title}</h2>
+            <h2 className="mb-1.5 text-title-md md:text-title-md2 font-bold text-black dark:text-white">
+              {title}
+            </h2>
           </div>
-          <p className="md:text-title-sm sm:text-medium" style={{ whiteSpace: 'pre-wrap' }}>
+          <p className="text-medium md:text-title-sm" style={{ whiteSpace: 'pre-wrap' }}>
             {subText}
           </p>
         </div>

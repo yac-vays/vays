5b8a49592bea81c59fb38f52344f67f502771480
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-01-24 17:19:17 +0100

    Add eslint security plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+const pluginSecurity = require('eslint-plugin-security');
 
 export default tseslint.config(
   eslint.configs.recommended,
-//   tseslint.configs.recommended,
-tseslint.configs.strict,
+  //   tseslint.configs.recommended,
+  tseslint.configs.strict,
+  pluginSecurity.configs.recommended
 );

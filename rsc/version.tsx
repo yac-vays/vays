811a7eba0c3d6dcb6a7e433238d03c78498cd0b7
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-02-04 16:59:07 +0100

    [Automated]: Increase version to 0.3rc17

--- rsc/version.tsx
+++ rsc/version.tsx
@@ -2,7 +2,7 @@
 // Automatically generated version file.
 // Please do not remove this file manually.
 
-const VAYS_VERSION = '0.3rc16';
+const VAYS_VERSION = '0.3rc17';
 export default VAYS_VERSION;
 
 

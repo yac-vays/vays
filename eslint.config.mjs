2a10e601e0e1f73d2328d44f9271d5ddb9f1ea21
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-02-05 15:03:18 +0100

    Update linting rules and packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,10 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-const pluginSecurity = require('eslint-plugin-security');
+//const pluginSecurity = require('eslint-plugin-security');
 
 export default tseslint.config(
   eslint.configs.recommended,
-  //   tseslint.configs.recommended,
-  tseslint.configs.strict,
-  pluginSecurity.configs.recommended
+  tseslint.configs.recommended,
+  tseslint.configs.strict
 );

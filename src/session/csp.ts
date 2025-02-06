bc0125024c141bffcc624d4af3ee5b6714dae489
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-02-06 08:48:54 +0100

    Update CSP to force using https over http for first- and thirdparty connections

--- src/session/csp.ts
+++ src/session/csp.ts
@@ -1,10 +1,18 @@
 import { AppConfig } from '../utils/types/config';
 
+/**
+ * Apply the CSP. Note this is not the entire policy!
+ * In the index.html file, there are default rules, like:
+ *
+ *  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+ * which forces the website to use HTTPS to connect to first and thirdparty resources.
+ * @param config
+ */
 export function generateCSP(config: AppConfig) {
   const cspMetaTag = document.createElement('meta');
   cspMetaTag.httpEquiv = 'Content-Security-Policy';
 
-  let urls = config.backends.map((v) => v.url).join(' ');
+  const urls = config.backends.map((v) => v.url).join(' ');
   const urlsAndOIDC = urls + ' ' + new URL(config.oidcConf.server).hostname;
 
   cspMetaTag.content = `default-src 'self'; 

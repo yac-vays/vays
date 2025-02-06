71804251bdbaeb50e1c2c86bb65e82d6a30c1ef5
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-02-06 15:56:33 +0100

    Use variable only for backend list

--- src/view/pages/DevInfo/index.tsx
+++ src/view/pages/DevInfo/index.tsx
@@ -1,35 +1,16 @@
 import { useEffect, useState } from 'react';
 import { getConfig } from '../../../model/config';
+import { YACBackend } from '../../../utils/types/config';
 import PageHeaderTitle from '../../thirdparty-based-components/PageTitle/PageHeaderTitle';
 
 const DevInfo = () => {
-  const [backends, setBackends] = useState<React.ReactNode>(undefined);
+  const [backends, setBackends] = useState<YACBackend[]>([]);
 
   useEffect(() => {
     (async () => {
       const conf = await getConfig();
       if (conf == null) return;
-      const jsx = [];
-      for (const be of conf.backends) {
-        jsx.push(
-          <p>
-            <span className="font-lg text-plainfont font-bold">{be.title + ' : '}</span>
-            <a href={be.url} target="_blank" style={{ color: 'lightblue' }}>
-              Documentation{' '}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="fill-current opacity-40 inline pl-0.5 cursor-pointer"
-                height="20px"
-                viewBox="0 -960 960 960"
-                width="20px"
-              >
-                <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
-              </svg>
-            </a>
-          </p>,
-        );
-      }
-      setBackends(jsx);
+      setBackends(conf.backends);
     })();
   }, []);
 
@@ -99,7 +80,31 @@ const DevInfo = () => {
               <br />
             </p>
             For the backend APIs, have a look at the documentation:
-            {backends}
+            {(function () {
+              const jsx = [];
+              for (const backend of backends) {
+                jsx.push(
+                  <p>
+                    <span className="font-lg text-plainfont font-bold">
+                      {backend.title + ' : '}
+                    </span>
+                    <a href={backend.url} target="_blank" style={{ color: 'lightblue' }}>
+                      Documentation{' '}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="fill-current opacity-40 inline pl-0.5 cursor-pointer"
+                        height="20px"
+                        viewBox="0 -960 960 960"
+                        width="20px"
+                      >
+                        <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
+                      </svg>
+                    </a>
+                  </p>,
+                );
+              }
+              return jsx;
+            })()}
           </div>
         </div>
       </section>

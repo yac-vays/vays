f0e08fe38d48d8498417a1f1027f1100c79f753f
CE-2 <193879258+CE-2@users.noreply.github.com>
2025-02-13 08:37:58 +0100

    Fix objectdiff

--- src/utils/objectdiff.ts
+++ src/utils/objectdiff.ts
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import _ from 'lodash';
 
-// Function to find the difference between two objects
+/**
+ * Finds the diff between two objects and returns it in human readable form.
+ * @param obj1 The old object
+ * @param obj2 The new object
+ * @returns
+ */
 export const getObjectDiff = (obj1: any, obj2: any) => {
   const diff: any = {};
 
@@ -14,14 +20,22 @@ export const getObjectDiff = (obj1: any, obj2: any) => {
   // Check for keys in the second object that are not in the first
   _.forOwn(obj2, (value, key) => {
     if (!_.has(obj1, key)) {
-      diff[key] = { oldValue: undefined, newValue: value };
+      diff[key] = ` -> removed (previously ${value})`;
+      //{ oldValue: undefined, newValue: value };
     }
   });
 
   return diff;
 };
 
-export function transformObjectUsingTitle(obj: any, schema: any) {
+/**
+ * Given an object and the json schema for it, try to replace as many keys with the
+ * corresponding title as possible.
+ * @param obj
+ * @param schema
+ * @returns
+ */
+export function transformObjectUsingTitle(obj: { [key: string]: any }, schema: any) {
   const transformedObj = _.cloneDeep(obj); // Create a deep clone to avoid mutation
 
   // Helper function to recursively process the schema and object
@@ -31,23 +45,22 @@ export function transformObjectUsingTitle(obj: any, schema: any) {
     }
 
     if (typeof obj === 'object' && obj !== null) {
-      const result = {};
+      const result: any = {};
       for (const [key, value] of Object.entries(obj)) {
         let propertySchema = _.get(schema, ['properties', key]);
 
         // Handle oneOf in schema
         if (!propertySchema && schema.oneOf) {
-          for (let subSchema of schema.oneOf) {
+          for (const subSchema of schema.oneOf) {
             propertySchema = _.get(subSchema, ['properties', key]);
             if (propertySchema) break;
           }
         }
 
         // If there is a title in the schema for this key, replace it
-        const newKey = propertySchema && propertySchema.title ? propertySchema.title : key;
+        const newKey: string = propertySchema && propertySchema.title ? propertySchema.title : key;
 
         // Recursively process nested objects or arrays
-        //@ts-ignore
         result[newKey] = processObject(value, propertySchema || {}, key);
       }
       return result;

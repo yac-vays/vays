46b5ddb8e7b7cfa745803566ae118fc5dd521a05
Raffael Nussbaumer <raffael.nussbaumer@id.ethz.ch>
2025-02-21 14:07:15 +0100

    First Draft for MultiLineTextControlRenderer

--- src/renderers/control/index.tsx
+++ src/renderers/control/index.tsx
@@ -1,21 +1,21 @@
-import BooleanControlRenderer, { BooleanControlTester } from './BooleanControlRenderer';
-import DateControl, { DateControlTester } from './DateControl';
-import EnumControl, { EnumControlTester } from './EnumControl';
-import MultipleChoiceRenderer, { MultipleChoiceTester } from './MultipleChoiceRenderer';
-import NumberControl, { NumberControlTester } from './NumberControl';
-import OneOfEnumControl, { OneOfEnumControlTester } from './OneOfEnumControl';
-import { specialRenderers } from './special';
 import TextControl, { TextControlTester } from './TextControlRenderer';
+import MultiLineTextControl, { MultiLineTextControlTester } from './MultiLineTextControlRenderer';
+import BooleanControlRenderer, { BooleanControlTester } from './BooleanControlRenderer';
+import OneOfEnumControl, { OneOfEnumControlTester } from './OneOfEnumControl';
+import EnumControl, { EnumControlTester } from './EnumControl';
+import DateControl, { DateControlTester } from './DateControl';
+import NumberControl, { NumberControlTester } from './NumberControl';
 import { VoidControl, VoidTester } from './VoidSchemaControl';
+import { specialRenderers } from './special';
 
 export const controlRenderers = [
   { tester: TextControlTester, renderer: TextControl },
+  { tester: MultiLineTextControlTester, renderer: MultiLineTextControl },
   { tester: BooleanControlTester, renderer: BooleanControlRenderer },
   { tester: OneOfEnumControlTester, renderer: OneOfEnumControl },
   { tester: EnumControlTester, renderer: EnumControl },
   { tester: DateControlTester, renderer: DateControl },
   { tester: NumberControlTester, renderer: NumberControl },
   { tester: VoidTester, renderer: VoidControl },
-  { tester: MultipleChoiceTester, renderer: MultipleChoiceRenderer },
   ...specialRenderers,
 ];

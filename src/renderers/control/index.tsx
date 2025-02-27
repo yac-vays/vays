69726465060956c85b47aab52db434319536e2ed
Raffael Nussbaumer <raffael.nussbaumer@id.ethz.ch>
2025-02-27 13:30:22 +0100

    moving imports to the correct index

--- src/renderers/control/index.tsx
+++ src/renderers/control/index.tsx
@@ -6,15 +6,9 @@ import OneOfEnumControl, { OneOfEnumControlTester } from './OneOfEnumControl';
 import TextControl, { TextControlTester } from './TextControlRenderer';
 import { VoidControl, VoidTester } from './VoidSchemaControl';
 import { specialRenderers } from './special';
-import MACAddressRenderer, { MacAddressRendererTester } from './special/MacAddressRenderer';
-import MultiLineTextControl, {
-  MultiLineTextControlTester,
-} from './special/MultiLineTextControlRenderer';
 
 export const controlRenderers = [
   { tester: TextControlTester, renderer: TextControl },
-  { tester: MultiLineTextControlTester, renderer: MultiLineTextControl },
-  { tester: MacAddressRendererTester, renderer: MACAddressRenderer },
   { tester: BooleanControlTester, renderer: BooleanControlRenderer },
   { tester: OneOfEnumControlTester, renderer: OneOfEnumControl },
   { tester: EnumControlTester, renderer: EnumControl },
--- src/renderers/control/special/index.tsx
+++ src/renderers/control/special/index.tsx
@@ -1,6 +1,8 @@
 import BigStringArray, { BigStringArrayTester } from './BigStringArray';
 import InfoBoxRenderer, { InfoBoxTester } from './InfoBoxRenderer';
 import ListAsStringRenderer, { ListAsStringTester } from './ListAsStringRenderer';
+import MACAddressRenderer, { MacAddressRendererTester } from './MacAddressRenderer';
+import MultiLineTextControl, { MultiLineTextControlTester } from './MultiLineTextControlRenderer';
 import PasswordRenderer, { PasswordRendererTester } from './PasswordRenderer';
 import SSHKeyRenderer, { SSHKeyRendererTester } from './SSHKeyRenderer';
 
@@ -10,4 +12,6 @@ export const specialRenderers = [
   { tester: SSHKeyRendererTester, renderer: SSHKeyRenderer },
   { tester: ListAsStringTester, renderer: ListAsStringRenderer },
   { tester: BigStringArrayTester, renderer: BigStringArray },
+  { tester: MultiLineTextControlTester, renderer: MultiLineTextControl },
+  { tester: MacAddressRendererTester, renderer: MACAddressRenderer },
 ];

b3c44230d09c7d97c10a1108e45a6a79aecaf9ef
Raffael Nussbaumer <raffael.nussbaumer@id.ethz.ch>
2025-05-06 14:23:45 +0200

    [Automated]: Increase version to 0.3rc34

--- rsc/version.tsx
+++ rsc/version.tsx
@@ -2,7 +2,7 @@
 // Automatically generated version file.
 // Please do not remove this file manually.
 
-const VAYS_VERSION = '0.3rc33';
+const VAYS_VERSION = '0.3rc34';
 export default VAYS_VERSION;
 
 

84048e2a99f10f6e5b06fb166f599c7f76ab6880
Manuel Maestinger <manuel.maestinger@inf.ethz.ch>
2026-05-27 16:54:12 +0200

    feat: use more feature-complete markdown-renderer

--- src/view/components/Markdown.tsx
+++ src/view/components/Markdown.tsx
@@ -1,34 +1,29 @@
 import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const MarkdownRender = ({ text }: { text: string | null | undefined }) => {
   if (!text) return <></>;
   return (
     <div className="whitespace-pre-wrap">
-      {text.split('\n').map((v) => (
-        <>
-          {/* {hasMultiLine ? '\n' : ''} */}
-          <Markdown
-            components={{
-              strong(props) {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                const { node, ...rest } = props;
-                return (
-                  <>
-                    <strong {...rest} />
-                  </>
-                );
-              },
-              a(props) {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                const { node, ...rest } = props;
-                return <a style={{ color: 'lightblue' }} target="_blank" {...rest} />;
-              },
-            }}
-          >
-            {v}
-          </Markdown>
-        </>
-      ))}
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          a({ href, children }) {
+            return (
+              <a
+                style={{ color: 'lightblue' }}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={href}
+              >
+                {children}
+              </a>
+            );
+          },
+        }}
+      >
+        {text}
+      </Markdown>
     </div>
   );
 };
